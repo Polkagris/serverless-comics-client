11f14a23a9bb864fdf5b840730ed2ac8089ea7e4
Arny <arne.n.bjerkaas@gmail.com>
2019-12-19 20:31:21 +0100

    Start login functionality

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
+import Login from './containers/Login';
 
 
 function Routes() {
     return (
         <Switch>
             <Route component={Home} path="/" exact />
+            <Route component={Login} path="/login" exact />
             { /* Finally, catch all unmatched routes */}
             <Route component={NotFound} />
         </Switch>
