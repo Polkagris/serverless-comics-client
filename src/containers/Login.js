8bcf8b376bb52d0b2b35d618f3c05b27cdb6328d
Arny <arne.n.bjerkaas@gmail.com>
2019-12-21 14:59:43 +0100

    Add custom hook to handle form fields

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,25 +1,34 @@
 import React, { useState } from 'react';
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "../styles/Login.css";
 import { Auth } from "aws-amplify";
+import LoaderButton from '../components/LoaderButton';
+import { useFormFields } from '../libs/hooksLib';
+
 
 function Login(props) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: ""
+    });
 
     const validateForm = () => {
-        return email.length > 0 && password.length > 0;
+        return fields.email.length > 0 && fields.password.length > 0;
     }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
+        setIsLoading(true);
+
         try {
-            await Auth.signIn(email, password);
+            await Auth.signIn(fields.email, fields.password);
             props.userHasAuthenticated(true);
             props.history.push("/");
         } catch (err) {
             alert(err.message);
+            setIsLoading(false);
         }
     }
 
@@ -32,21 +41,26 @@ function Login(props) {
                     <FormControl
                         autoFocus
                         type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
+                        value={fields.email}
+                        onChange={handleFieldChange}
                     />
                 </FormGroup>
                 <FormGroup controlId="password" bsSize="large">
                     <ControlLabel>Password</ControlLabel>
                     <FormControl
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
+                        value={fields.password}
+                        onChange={handleFieldChange}
                         type="password"
                     />
                 </FormGroup>
-                <Button block bsSize="large" disabled={!validateForm()} type="submit">
+                <LoaderButton
+                    block bsSize="large"
+                    disabled={!validateForm()}
+                    type="submit"
+                    isLoading={isLoading}
+                >
                     Login
-                </Button>
+                </LoaderButton>
             </form>
         </div>
     )
